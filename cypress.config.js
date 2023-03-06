1a2cf9e847f2869fd58d8e842a5aa74a58d1eb88
Johnny Kamigashima <jrrhack@gmail.com>
2023-03-05 21:21:25 -0300

    Primeiro upload

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'z7r4bs',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://saucedemo.com',
+    retries: 2
+  },
+});
